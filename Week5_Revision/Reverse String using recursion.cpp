c8976803c5c11484860b73c4fbfb722eb8ad84e3
sk9245 <90946291+sk9245@users.noreply.github.com>
2021-10-19 13:07:21 -0400

    Reverse String

---
+++ Week5_Revision/Reverse String using recursion.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-string/
+
+// Solve Using recursion
+class Solution {
+public:
+    
+    void ans(vector<char>& s, int left, int right){
+        if(left>=right)
+            return;
+        swap(s[left], s[right]);
+        left++;
+        right--;
+        ans(s, left, right);
+    }
+    void reverseString(vector<char>& s) {
+        ans(s, 0, s.size() - 1);
+        
+    }
+};
