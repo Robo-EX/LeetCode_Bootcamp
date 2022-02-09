8283b8b100fa72d8c28f11aa2db513f6c675f0a3
Bhavik Mukesh Panchal <bmp7963@nyu.edu>
2022-02-09 12:41:00 -0500

    test

--- Week1_Array-Sorting/Squares_of_Sorted_Array.cpp
+++ Week1_Array-Sorting/Squares_of_Sorted_Array.cpp
@@ -9,7 +9,7 @@ public:
         int left = 0;
         int right = n - 1;
         for(int i = n - 1; i >= 0 ; i--) { 
-            int temp;sdsd
+            int temp;
             if(abs(nums[left]) < abs(nums[right])){
                 temp = nums[right];
                 right--;
